3c9570615c7035a3862431d56a732e51a205279e
myehrajat <31361589+myehrajat@users.noreply.github.com>
2018-12-10 13:42:05 +0330

    bugfix: sys car not worked
    post_id used instead product and untill now jalali date only supported and gergorian date will fail

--- rentit_get_sys_cars.php
+++ rentit_get_sys_cars.php
@@ -31,7 +31,7 @@ include_once('save_options.php');
 
 
 
-function RentIt_Get_Sys_Cars_get_cars( $product ) {
+function RentIt_Get_Sys_Cars_get_cars( $post_id) {
 
 	 if(!isset($start_date)){
 	 }
@@ -44,16 +44,13 @@ function RentIt_Get_Sys_Cars_get_cars( $product ) {
 	static $gregorian_to_jalali;
 	if($gregorian_to_jalali != 'done'){
 		$gregorian_to_jalali = 'done';
-		$end_date = sst_simple_gregorian_to_jalali( $_GET['end_date']);
-		$start_date = 	 sst_simple_gregorian_to_jalali( $_GET['start_date']);
+		//$end_date = sst_simple_gregorian_to_jalali( $_GET['end_date']);
+		//$start_date = sst_simple_gregorian_to_jalali( $_GET['start_date']);
 
 	}
-//var_dump($_GET['start_date']);
-//var_dump($_GET['end_date']);
-//var_dump($start_date);
-//var_dump($end_date);
-//	die;
-	$sys_id = $product->get_attribute( '_rentit_sys_car_id' );
+
+	$sys_id = get_post_meta( $post_id, '_rentit_sys_car_id',true  );
+
 	if ( !$sys_mojoodi ) {
 
 		$sys_mojoodi = json_decode( file_get_contents( 'https://sys.ejarehkhodro.com/wp-content/plugins/jqwidget-custom/json.php?list=mojoodi&start=' . urlencode( $start_date ) . '&end=' . urlencode($end_date) . '&min=0&max=99999999999999&vo=7&psw=ehrajat1363&rawdata=raw' ) );
